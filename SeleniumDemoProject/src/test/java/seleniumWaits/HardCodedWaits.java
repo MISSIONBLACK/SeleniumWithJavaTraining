006888b4e8b2494d720ea58e43f49aaa79e9454e
AKAGAR <akash.a.agarwal@us.mcd.com>
2024-11-24 10:20:36 +0530

    Waits in Selenium

---
+++ SeleniumDemoProject/src/test/java/seleniumWaits/HardCodedWaits.java
@@ -0,0 +1,39 @@
+package seleniumWaits;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class HardCodedWaits {
+
+	/*
+	 * Test Case -- Google Search Text
+	 */
+
+	@Test
+	public void googleSearchTest() throws InterruptedException {
+
+		System.setProperty("webdriver.chrome.driver",
+				"C:/Selenium Training/eclipse-workspace/SeleniumDemoProject/drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+
+		driver.manage().window().maximize();
+
+		driver.get("https://www.google.com/");
+
+		WebElement searchBox = driver.findElement(By.xpath("//textarea[@title='Search' and @role='combobox']"));
+		searchBox.sendKeys("Selenium Webdriver");
+		Thread.sleep(4000);
+		
+		WebElement searchResult = driver.findElement(By.xpath("//li[@role='presentation'][4]"));
+		searchResult.click();
+		
+		Thread.sleep(2000);
+		
+		driver.quit();
+
+	}
+
+}
